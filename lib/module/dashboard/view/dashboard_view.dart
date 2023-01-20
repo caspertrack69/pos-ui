ad4c41f53919ba3ed7bd889d421c90456deb3dfb
caspertrack69 <ar886069@gmail.com>
2023-01-21 02:13:04 +0700

    update logout

--- lib/module/dashboard/view/dashboard_view.dart
+++ lib/module/dashboard/view/dashboard_view.dart
@@ -3,6 +3,7 @@ import 'package:pos_ui/constants.dart';
 import 'package:pos_ui/core.dart';
 import '../controller/dashboard_controller.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:pos_ui/module/login/view/login_view.dart';
 
 class DashboardView extends StatefulWidget {
   const DashboardView({Key? key}) : super(key: key);
@@ -14,7 +15,20 @@ class DashboardView extends StatefulWidget {
       appBar: AppBar(
         title: const Text("Dashboard"),
         backgroundColor: kBlue,
-        actions: const [],
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 20),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).pushReplacement(new MaterialPageRoute(
+                    builder: (BuildContext context) => LoginView(
+                          screenHeight: 0,
+                        )));
+              },
+              child: Icon(Icons.logout_outlined),
+            ),
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Container(
@@ -119,6 +133,112 @@ class DashboardView extends StatefulWidget {
                     ),
                   ],
                 ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              LayoutBuilder(
+                builder: (context, constraint) {
+                  List menus = [
+                    {
+                      "icon":
+                          "https://cdn-icons-png.flaticon.com/128/878/878052.png",
+                      "label": "Burger",
+                      "onTap": () {},
+                    },
+                    {
+                      "icon":
+                          "https://cdn-icons-png.flaticon.com/128/3595/3595455.png",
+                      "label": "Pizza",
+                      "onTap": () {},
+                    },
+                    {
+                      "icon":
+                          "https://cdn-icons-png.flaticon.com/128/2718/2718224.png",
+                      "label": "Noodles",
+                      "onTap": () {},
+                    },
+                    {
+                      "icon":
+                          "https://cdn-icons-png.flaticon.com/128/8060/8060549.png",
+                      "label": "Meat",
+                      "onTap": () {},
+                    },
+                    {
+                      "icon":
+                          "https://cdn-icons-png.flaticon.com/128/454/454570.png",
+                      "label": "Soup",
+                      "onTap": () {},
+                    },
+                    {
+                      "icon":
+                          "https://cdn-icons-png.flaticon.com/128/2965/2965567.png",
+                      "label": "Dessert",
+                      "onTap": () {},
+                    },
+                    {
+                      "icon":
+                          "https://cdn-icons-png.flaticon.com/128/2769/2769608.png",
+                      "label": "Drink",
+                      "onTap": () {},
+                    },
+                    {
+                      "icon":
+                          "https://cdn-icons-png.flaticon.com/128/1037/1037855.png",
+                      "label": "Others",
+                      "onTap": () {},
+                    },
+                  ];
+
+                  return Wrap(
+                    children: List.generate(
+                      menus.length,
+                      (index) {
+                        var item = menus[index];
+
+                        var size = constraint.biggest.width / 4;
+
+                        return SizedBox(
+                          width: size,
+                          height: size,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              foregroundColor: Colors.blueGrey,
+                              animationDuration:
+                                  const Duration(milliseconds: 1000),
+                              backgroundColor: Colors.transparent,
+                              splashFactory: InkSplash.splashFactory,
+                              shadowColor: Colors.transparent,
+                              elevation: 0.0,
+                            ),
+                            onPressed: () => item["onTap"](),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.network(
+                                  item["icon"],
+                                  width: 30.0,
+                                ),
+                                const SizedBox(
+                                  height: 6.0,
+                                ),
+                                Text(
+                                  "${item["label"]}",
+                                  textAlign: TextAlign.center,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: const TextStyle(
+                                    fontSize: 11.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                },
               ),
             ],
           ),
